fcbe37a5657bae3372084b72987a36a1969dbf4e
Kangwon Lee <kangwon.lee@kpu.ac.kr>
2016-01-01 21:18:51 +0900

    /anaconda/bin/python /Users/programmer/Dev/git_repository/NAIST_DeepLearning/symbolic_back_propagation.py Traceback (most recent call last):   File "/Users/programmer/Dev/git_repository/NAIST_DeepLearning/symbolic_back_propagation.py", line 45, in <module>     main()   File "/Users/programmer/Dev/git_repository/NAIST_DeepLearning/symbolic_back_propagation.py", line 38, in main     print(sp.diff(in_k, sp.Matrix(wjk).tolist()))   File "/anaconda/lib/python3.5/site-packages/sympy/core/function.py", line 1638, in diff     return Derivative(f, *symbols, **kwargs)   File "/anaconda/lib/python3.5/site-packages/sympy/core/function.py", line 992, in __new__     if not variables[-1].is_Integer or len(variables) == 1: AttributeError: 'list' object has no attribute 'is_Integer'
    Process finished with exit code 1

--- symbolic_back_propagation.py
+++ symbolic_back_propagation.py
@@ -4,7 +4,8 @@ import numpy as np
 
 def matrix(s, n, m):
     # ref: Brendan Wood, MRocklin, Amelio Vazquez-Reina, Automatically populating matrix elements in SymPy, http://stackoverflow.com/questions/6877061/automatically-populating-matrix-elements-in-sympy 2013 Dec 17 (Accessed 2016 01 01).
-    return sp.Matrix(n, m, lambda i,j:sp.var('%s_%d%d' % (s, i+1,j+1)))
+    # return sp.Matrix(n, m, lambda i,j:sp.var('%s_%d%d' % (s, i+1,j+1)))
+    return sp.MatrixSymbol(s, n, m)
 
 
 def sigmoid_z(z):
@@ -20,7 +21,7 @@ def sigmoid_z(z):
             new_row.append(sp.simplify(1.0 / (1 + sp.exp(-z_row[j]))))
         result.append(new_row)
 
-    return result
+    return sp.Matrix(result)
 
 
 def main():
@@ -32,8 +33,9 @@ def main():
     wij = matrix('wij', i, j)
     wjk = matrix('wjk', j, k)
 
-    in_k = sp.simplify(wjk.T * wij.T * xi)
-    print(in_k.shape)
+    in_k = sp.Matrix(sp.simplify(wjk.T * wij.T * xi)).tolist()
+
+    print(sp.diff(in_k, sp.Matrix(wjk).tolist()))
 
     y_k = sigmoid_z(in_k)
     print(y_k)
