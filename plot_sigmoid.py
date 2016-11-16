ef965b1ebc59643e6d6e61c95eccbae42c629ec0
KangWon LEE <kangwon@gmail.com>
2016-11-16 17:49:07 +0900

    import x from sympy.abc added proc_sympy_function() added legend to plot

--- plot_sigmoid.py
+++ plot_sigmoid.py
@@ -1,30 +1,40 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import sympy as sp
+from sympy.abc import x
 
 
 def get_integrated_sigmoid_sympy():
-    sigmoid, x = get_sigmoid_function_sympy()
-
-    return [x, sp.integrate(sigmoid, x), 'numpy']
+    sigmoid = get_sigmoid_function_sympy()
+    return sp.integrate(sigmoid, x)
 
 
 def get_sigmoid_function_sympy():
-    x = sp.symbols('x')
     sigmoid = 1 / (1 + sp.exp(-x))
-    return [x, sigmoid, 'numpy']
+    return sigmoid
+
+
+def proc_sympy_function(x_array, expression, label, lib='numpy'):
+    # Numeric Computation, Sympy documentation, 2016, [Online] Available
+    # http://docs.sympy.org/dev/modules/numeric-computation.html
+    f = sp.lambdify(x, expression, lib)
+    y_f = f(x_array)
+    plt.plot(x_array, y_f, label=label)
 
 
 def main(min_x=-8, max_x=8):
-    # Numeric Computation, Sympy documentation, 2016, [Online] Available
-    # http://docs.sympy.org/dev/modules/numeric-computation.html
-    sigmoid = sp.lambdify(*get_sigmoid_function_sympy())
+    x_array = np.linspace(min_x, max_x)
 
-    x = np.linspace(min_x, max_x)
-    y = sigmoid(x)
-    plt.plot(x, y)
+    process_them = [
+        {'label': 'sigmoid', 'f': get_sigmoid_function_sympy()},
+        {'label': 'integrated sigmoid', 'f': get_integrated_sigmoid_sympy()},
+    ]
+
+    for d in process_them:
+        proc_sympy_function(x_array, d['f'], d['label'])
 
     plt.grid(True)
+    plt.legend(loc=0)
     plt.savefig('sigmoid.png', dpi=300)
 
 
