68ad438ac273203bc52910841d4c45221b306c54
KangWon LEE <kangwon@gmail.com>
2016-11-16 17:27:52 +0900

    added sigmoid function & integrated sigmoid function in sympy converted into a function using lambdify-numpy # Numeric Computation, Sympy documentation, 2016, [Online] Available # http://docs.sympy.org/dev/modules/numeric-computation.html

--- plot_sigmoid.py
+++ plot_sigmoid.py
@@ -1,12 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import sympy as sp
 
 
-def sigmoid(x):
-    return 1.0 / (1.0 + np.exp(-x))
+def get_integrated_sigmoid_sympy():
+    sigmoid, x = get_sigmoid_function_sympy()
+
+    return [x, sp.integrate(sigmoid, x), 'numpy']
+
+
+def get_sigmoid_function_sympy():
+    x = sp.symbols('x')
+    sigmoid = 1 / (1 + sp.exp(-x))
+    return [x, sigmoid, 'numpy']
 
 
 def main(min_x=-8, max_x=8):
+    # Numeric Computation, Sympy documentation, 2016, [Online] Available
+    # http://docs.sympy.org/dev/modules/numeric-computation.html
+    sigmoid = sp.lambdify(*get_sigmoid_function_sympy())
+
     x = np.linspace(min_x, max_x)
     y = sigmoid(x)
     plt.plot(x, y)
